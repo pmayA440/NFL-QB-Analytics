9ff26d5678a54b45dc62fa0db00f3baf6869c677
Philip M <philipwaltermay@gmail.com>
2019-03-30 13:30:52 -0500

    update app file, cleanup files

--- app.py
+++ app.py
@@ -22,43 +22,43 @@ def index():
     """Return the homepage."""
     return render_template("index.html")
 
-# # All-Data route
-# @app.route("/all-data")
-# def names():
+# All-Data route
+@app.route("/all-data")
+def names():
 
-#     # Use Pandas to read in data
-#     df = pd.read_csv(path)
-
-#     # Return a list of the data
-#     data = {
-#         'Name': df['Name'].values.tolist(),
-#         'Year': df['Year'].values.tolist(),
-#         'Season': df['Season'].values.tolist(),
-#         'Week': df['Week'].values.tolist(),
-#         'Outcome': df['Outcome'].values.tolist(),
-#         'Completion Percentage': df['Completion Percentage'].values.tolist(),
-#         'Passing Yards': df['Passing Yards'].values.tolist(),
-#         'TD Passes': df['TD Passes'].values.tolist(),
-#     }
-#     return jsonify(data)
-
-# Group route
-@app.route("/groups")
-def sums():
-
-    # # Use Pandas to read in data
+    # Use Pandas to read in data
     df = pd.read_csv(path)
 
-    # Return a list of the data
+    # data = df.to_json
+
+    #Return a list of the data
     data = {
-        'Year': df['Year'].values.todict(),
-        'Name': df['Name'].values.todict(),
-        'Season': df['Season'].values.todict(),
-        'Passing Yards': df['Passing Yards'].values.todict(),
-        'TD Passes': df['TD Passes'].values.todict(),
-        'Wins': df['Wins'].values.todict(),
+        'Year': df['Year'].values.tolist(),
+        'Name': df['Name'].values.tolist(),
+        'Season': df['Season'].values.tolist(),
+        'Passing Yards': df['Passing Yards'].values.tolist(),
+        'TD Passes': df['TD Passes'].values.tolist(),
+        'Wins': df['Wins'].values.tolist(),
     }
     return jsonify(data)
+
+# # Group route
+# @app.route("/groups")
+# def sums():
+
+#     # # Use Pandas to read in data
+#     df = pd.read_json(path)
+
+#     # # Return a list of the data
+#     # data = {
+#     #     'Year': df['Year'].values.todict(),
+#     #     'Name': df['Name'].values.todict(),
+#     #     'Season': df['Season'].values.todict(),
+#     #     'Passing Yards': df['Passing Yards'].values.todict(),
+#     #     'TD Passes': df['TD Passes'].values.todict(),
+#     #     'Wins': df['Wins'].values.todict(),
+#     # }
+#     return jsonify(df)
 
 
 if __name__ == "__main__":
