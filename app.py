918c7d70ed7a493a14d9d99c2143ef52ea189d25
Philip M <philipwaltermay@gmail.com>
2019-04-02 19:53:22 -0500

    Add SQL DB code

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # Database Setup
 cwd = os.getcwd()
 f = "Assets\\Data_Files"
-file = "Cleaned_QB_data_groups.csv"
+file = "data.json"
 path = os.path.join(cwd, f, file)
 
 
@@ -27,38 +27,46 @@ def index():
 def names():
 
     # Use Pandas to read in data
-    df = pd.read_csv(path)
+    df = pd.read_json(path)
 
-    # data = df.to_json
-
-    #Return a list of the data
     data = {
-        'Year': df['Year'].values.tolist(),
         'Name': df['Name'].values.tolist(),
+        'Year': df['Year'].values.tolist(),
         'Season': df['Season'].values.tolist(),
+        'Wins': df['Wins'].values.tolist(),
         'Passing Yards': df['Passing Yards'].values.tolist(),
         'TD Passes': df['TD Passes'].values.tolist(),
-        'Wins': df['Wins'].values.tolist(),
     }
+
     return jsonify(data)
 
-# # Group route
-# @app.route("/groups")
-# def sums():
+# All-Data route
+@app.route("/json-data")
+def json():
 
-#     # # Use Pandas to read in data
-#     df = pd.read_json(path)
+    # Use Pandas to read in data
+    df = pd.read_json(path)
 
-#     # # Return a list of the data
-#     # data = {
-#     #     'Year': df['Year'].values.todict(),
-#     #     'Name': df['Name'].values.todict(),
-#     #     'Season': df['Season'].values.todict(),
-#     #     'Passing Yards': df['Passing Yards'].values.todict(),
-#     #     'TD Passes': df['TD Passes'].values.todict(),
-#     #     'Wins': df['Wins'].values.todict(),
-#     # }
-#     return jsonify(df)
+    data = []
+    # data = {
+    #     'Name': df['Name'].values.tolist(),
+    #     'Year': df['Year'].values.tolist(),
+    #     'Season': df['Season'].values.tolist(),
+    #     'Wins': df['Wins'].values.tolist(),
+    #     'Passing Yards': df['Passing Yards'].values.tolist(),
+    #     'TD Passes': df['TD Passes'].values.tolist(),
+    # }
+    for d in df:
+        qb_dict = {}
+        qb_dict["Name"] = df['Name']
+        qb_dict["Year"] = df['Year']
+        qb_dict["Season"] = df['Season']
+        qb_dict["Wins"] = df['Wins']
+        qb_dict["Passing_Yards"] = df['Passing Yards']
+        qb_dict["TD_Passes"] = df['TD Passes']
+        data.append(qb_dict)
+
+    return jsonify(data)
 
 
 if __name__ == "__main__":
